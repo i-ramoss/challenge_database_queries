dff7d9d198ebc61f0b843151cb00498c7e69a763
Ian Ramos <ianramossantos@hotmail.com>
2021-04-10 12:49:48 -0300

    chore: implement user repository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,26 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({ id: user_id }, { relations: ['games'] });
+
+    if (!user) throw new Error('User not found!');
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users ORDER BY first_name ASC');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      SELECT * 
+      FROM users 
+      WHERE LOWER(first_name) = LOWER('${first_name}')
+      AND LOWER(last_name) = LOWER('${last_name}')
+    `);
   }
 }
